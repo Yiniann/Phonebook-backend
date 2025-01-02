73a1fa320b4eaf1ac356aaa74f49b75318821266
y1niann <i@en1an.com>
2025-01-02 02:47:18 -0500

    build-ui

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 let persons = [
     { 
@@ -81,7 +82,7 @@ app.post('/api/persons',(request,response)=>{
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
