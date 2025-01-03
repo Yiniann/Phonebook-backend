338f53a1552423c76ef14c6605cfd4a1358d608f
y1niann <i@en1an.com>
2025-01-03 05:07:31 -0500

    增加联系人

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
   Phonebook.find({}).then(persons=>{
     response.json(persons)
+    .catch(error=>next(error))
   })
 })
 //信息
@@ -29,12 +30,14 @@ app.get('/api/persons/:id',(request,response)=>{
   Phonebook.findById(request.params.id).then(person=>{
     response.json(person)
   })
+  .catch(error=>next(error))
 })
 //删除联系人
 app.delete('/api/persons/:id',(request,response)=>{
     Phonebook.findByIdAndDelete(request.params.id).then(()=>{
       response.status(204).end()
     })
+    .catch(error=>next(error))
 })
 //增加联系人
 app.post('/api/persons',(request,response)=>{
@@ -62,7 +65,20 @@ app.post('/api/persons',(request,response)=>{
     person.save().then(savePersons=>{
       response.json(savePersons)
     })
+    .catch(error=>next(error))
 })
+//修改联系人
+app.put('/api/persons/:id',(request,response)=>{
+  const body = request.body
+  const person ={
+    name:body.name,
+    number:body.number}
+    Phonebook.findByIdAndUpdate(request.params.id,person,{new:true}).then(updatePerson=>{
+      response.json(updatePerson)
+    })
+    .catch(error=>next(error))
+})
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
